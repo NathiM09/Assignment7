007f357178436ecee738531367b126c16c250bcf
Anathi Matinise <amatinise@my365-uwc.ac.za>
2020-11-20 11:49:17 +0200

    new update

--- assignment 7/assignment 7/assignment 7/MainPage.xaml.cs
+++ assignment 7/assignment 7/assignment 7/MainPage.xaml.cs
@@ -19,5 +19,30 @@ namespace assignment_7
         {
 
         }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_3(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_4(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_5(object sender, EventArgs e)
+        {
+
+        }
     }
 }
